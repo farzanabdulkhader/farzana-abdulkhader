cea560589291e8f18df606a7db0682a8d2f4247e
Farzana Abdulkhader <farzanabdulkhader@gmail.com>
2023-12-28 14:59:26 +0530

    fix bg issue

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -6,10 +6,10 @@ function Background({ children }) {
   return (
     <>
       <div className="overlay1"></div>
-      <div className="pattern bg-[url('bg/bg-pattern.jpg')]"></div>
+      <div className="pattern bg-[url('/static/bg/bg-pattern.jpg')]"></div>
       <RippleBg />
       <div className="overlay2"></div>
-      <div className="background-image bg-[url('bg/bg.jpg')]"></div>
+      <div className="background-image bg-[url('/static/bg/bg.jpg')]"></div>
       {children}
     </>
   );
