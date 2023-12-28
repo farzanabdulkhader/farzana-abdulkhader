2ec320fe41f0f3c74a20ad7e9140b7ff60ea4fd7
Farzana Abdulkhader <farzanabdulkhader@gmail.com>
2023-12-28 13:32:50 +0530

    test bg image

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -6,10 +6,10 @@ function Background({ children }) {
   return (
     <>
       <div className="overlay1"></div>
-      <div className="pattern"></div>
+      <div className="pattern bg-[url('bg/bg-pattern.jpg')]"></div>
       <RippleBg />
       <div className="overlay2"></div>
-      <div className="background-image"></div>
+      <div className="background-image bg-[url('bg/bg.jpg')]"></div>
       {children}
     </>
   );
